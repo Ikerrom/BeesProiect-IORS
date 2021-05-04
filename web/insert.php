217008c928b2e60069dace918d1067017aa56dd5
Ikerrom <55279037+Ikerrom@users.noreply.github.com>
2021-05-04 12:00:16 +0200

    Update insert.php

--- web/insert.php
+++ web/insert.php
@@ -1,18 +1,18 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "myDB";
+$username = "root";
+$password = "dam1";
+$dbname = "bees_project";
 
-// Create connection
+
 $conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
+
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
+$sql = "INSERT INTO test (id)
+VALUES ('4')";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
