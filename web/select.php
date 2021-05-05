6ab608c96436e17243e37768f07990bd93ef1f5b
Ikerrom <55279037+Ikerrom@users.noreply.github.com>
2021-05-05 12:54:04 +0200

    Update select.php

--- web/select.php
+++ web/select.php
@@ -11,7 +11,7 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT dni FROM Personas";
+$sql = "SELECT dni FROM Persona";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
