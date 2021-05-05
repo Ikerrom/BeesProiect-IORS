21e04d000336492e63816be568c98b034f4171f2
Ikerrom <55279037+Ikerrom@users.noreply.github.com>
2021-05-05 12:55:27 +0200

    Update select.php

--- web/select.php
+++ web/select.php
@@ -11,9 +11,9 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT dni FROM Persona";
-$result = $conn->query($sql);
+$sql = "SELECT dni, dia_reservado, lata_id, dia_dereserva FROM Reservas INNER JOIN (Personas ON Reservas.dni = Personas.dni) INNER JOIN Latas ON Reservas.lata_id = Latas.lata_id";
 
+$result = $conn->query($sql);
 if ($result->num_rows > 0) {
   while($row = $result->fetch_assoc()) {
      echo "DNI: " . "<b>" . $row["dni"] . "</b>" . "Dia reservado: " . $row["dia_reservado"] . "Id lata: " . $row["lata_id"] . "Dia de reserva: " . $row["dia_dereserva"] . "<br>";
