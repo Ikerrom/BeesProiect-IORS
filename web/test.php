3e903e1de35b19fa8e584a675b5a434d13b33ee3
Ikerrom <55279037+Ikerrom@users.noreply.github.com>
2021-05-05 13:27:27 +0200

    Update test.php

--- web/test.php
+++ web/test.php
@@ -1,32 +1,39 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "dam1";
-$dbname = "bees_project";
+<!DOCTYPE html>
+<html>
+    <body>
+        <form>
+                <label for="cars">Choose a car:</label>
+                <select name="test">
+                  
+                <?php
+                $servername = "localhost";
+                $username = "root";
+                $password = "dam1";
+                $dbname = "bees_project";
 
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+                $conn = new mysqli($servername, $username, $password, $dbname);
 
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+                if ($conn->connect_error) {
+                  die("Connection failed: " . $conn->connect_error);
+                }
 
-$sql = "SELECT * FROM Latas,Personas";
+                $sql = "SELECT * FROM Latas,Personas";
 
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-  while($row = $result->fetch_assoc()) {
-           <form>
-        <label for="cars">Choose a car:</label>
-        <select name="test">
-          <option value="volvo">Volvo</option>
-          <option value="saab">Saab</option>
-        </select>
-      </form>
-  }
+                $result = $conn->query($sql);
+                if ($result->num_rows > 0) {
+                  while($row = $result->fetch_assoc()) {
+                    ?>
+                          <option value="<?php echo $row["dni"] . '">' . $row["dni"]?></option>
+                  }
 
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
+                } else {
+                  echo "0 results";
+                }
+                $conn->close();
+                ?>
+
+                </select>
+         </form>
+    </body>
+</html>
