17eba07b7538fc0a0c1489088e3b49d50df11969
Ikerrom <55279037+Ikerrom@users.noreply.github.com>
2021-05-06 09:40:33 +0200

    Add files via upload

---
+++ web/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>ERLETE</title>
+		<style>
+			
+			body{
+				margin:0;
+			}
+
+			.topbar{
+				width: 100%;
+				height: 8vh;
+				background-color: #ffd000;
+				top:0;
+				position: sticky;
+				border-bottom:1vh solid #ffbb00;
+			}
+
+			.title{
+				text-align: center;
+				font-size: 400%;
+				background-color:#ffd000;
+				font-weight: bold;
+				color: #ff9900;
+			}
+		</style>
+	</head>
+	<body>
+		<div class="title">ERLETE</div>
+		<div class="topbar">
+		</div>
+	</body>
+</html>
