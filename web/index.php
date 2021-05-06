0358b032ecf5449971fe2717600002568b8e0177
Ikerrom <55279037+Ikerrom@users.noreply.github.com>
2021-05-06 09:49:27 +0200

    Add files via upload

--- web/index.php
+++ web/index.php
@@ -1,30 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
+		<link rel="stylesheet" href="css.css">
 		<title>ERLETE</title>
-		<style>
-			
-			body{
-				margin:0;
-			}
 
-			.topbar{
-				width: 100%;
-				height: 8vh;
-				background-color: #ffd000;
-				top:0;
-				position: sticky;
-				border-bottom:1vh solid #ffbb00;
-			}
-
-			.title{
-				text-align: center;
-				font-size: 400%;
-				background-color:#ffd000;
-				font-weight: bold;
-				color: #ff9900;
-			}
-		</style>
 	</head>
 	<body>
 		<div class="title">ERLETE</div>
