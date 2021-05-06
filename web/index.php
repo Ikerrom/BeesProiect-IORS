d2002785a4a4972c949397b5ad1c93f367e100b1
Ikerrom <55279037+Ikerrom@users.noreply.github.com>
2021-05-06 14:21:12 +0200

    Update index.php

--- web/index.php
+++ web/index.php
@@ -9,23 +9,27 @@
 
 		<div class="title">ERLETE</div>
 		<div class="topbar">
-			<form action="login.php">
+			
 			    
 			    <?php
 			    include(testlogin.php);
 			    session_start();
 					if (isset($_SESSION['erablitzailea_a_g'])) 
-								{
-				?>
-									 <input class="buttonP" type="submit" value="Log Off"/>
+					{
+				?>			
+				<form action="singout.php">
+				<input type="submit" value="Sesioa itxi"/>
+				</form>
 				<?php
-								} else{
-									?>
-									<input class="buttonP" type="submit" value="Log In"/>
-									<?php
-								}
+					} else{
+					?>
+					<form action="login.php">
+					<input class="buttonP" type="submit" value="Log In"/>
+					</form>
+					<?php
+					}
 				?>
-			</form>
+			
 			<button class="buttonP">About Us</button>
 		</div>
 
