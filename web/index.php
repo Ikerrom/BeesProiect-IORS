81e0702bd10cd9e78ed076d628be207465f6fc96
Ikerrom <ikerrome1@gmail.com>
2021-05-10 13:07:19 +0200

    page changes

--- web/index.php
+++ web/index.php
@@ -4,11 +4,13 @@
  ?>
 <html>					
 	<head>
+
 		<link rel="stylesheet" href="css.css">
 		<title>ERLETE</title>
+		<link rel="preconnect" href="https://fonts.gstatic.com">
+		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
 	</head>
 	<body>
-
 		<div class="title">
 			
 			    <?php
@@ -21,50 +23,54 @@
 				$result=mysqli_query($link, "select nombre from Personas where dni = '$dni'"); 
 
 				$imprimir = mysqli_fetch_array($result);
-					?>	
-					<div class="perfil">
-						<img src="perfil.png" style="width:10vh;height: 10vh;float:left;">
-						<p class="textstyle">User: <?php echo $imprimir['nombre'];?></p>
-						<p class="textstyle">DNI: <?php echo $dni;?></p>
+					?>
+
+						<div class="perfil">
+						<img class="perfilimage" src="resources/Images/perfil.png">
+						<p>User: <?php echo $imprimir['nombre'];?></p>
+						<p>DNI: <?php echo $dni;?></p>
+
 					</div>
+
 					<?php
 					}
 					?>
-			
-		<p class="titletext">ERLETE</p></div>
+
+				<p class="titletext">ERLETE</p>
+		</div>
 		
 		<div class="topbar">
-			
 			    <?php
 					if (isset($_SESSION['erablitzailea_a_g'])) 
 					{
-				?>			
+				?>
 				<form action="singout.php">
-					<input class="buttonT" type="submit" value="Log Out"/>
+					<input class="buttonT" type="submit" value="LOG OUT"/>
 				</form>
 
 				<form action="account.php">
-					<input class="buttonT" type="submit" value="Your Account"/>
+					<input class="buttonT" type="submit" value="YOUR ACCOUNT"/>
 				</form>
 
 				<form action="booking.php">
-					<input class="buttonT" type="submit" value="Booking"/>
+					<input class="buttonT" type="submit" value="BOOKING"/>
 				</form>
 
 				<?php
 					}else{
 					?>
 					<form action="login.php">
-						<input class="buttonT" type="submit" value="Log In"/>
+						<input class="buttonT" type="submit" value="LOG IN"/>
 					</form>
 					<?php
 					}
 				?>
 			<form action="about.php">
-						<input class="buttonT" type="submit" value="About Us"/>
+						<input class="buttonT" type="submit" value="ABOUT US"/>
 					</form>
 			
 		</div>
+
 
 	</body>
 </html>
