730242d0c9f686f62ebaf6eb42a7103588715985
Ikerrom <55279037+Ikerrom@users.noreply.github.com>
2021-05-11 11:49:54 +0200

    Add files via upload

--- web/testlogin.php
+++ web/testlogin.php
@@ -1,19 +1,15 @@
-	<?php
-		include("test_connect_db.php");
-		$user = $_POST["Usuario"];
-		$password = $_POST["Password"];
-		$link =  ConnectDataBase();
-
-		$result=mysqli_query($link, "select dni,contraseña from Personas where dni = '$user' and contraseña = '$password'");
-
-		if (mysqli_num_rows($result) == 0)
-		{
-
-			header("Location:login.php?incorrecto=si");
-
-		}else{
-			session_start();
-			$_SESSION['erablitzailea_a_g'] = $user;
-			header("Location:index.php");
-		}
-	?>
+<?php
+	include("test_connect_db.php");
+	$user = $_POST["Usuario"];
+	$password = $_POST["Password"];
+	$link =  ConnectDataBase();
+	$result=mysqli_query($link, "select dni,contraseña from Personas where dni = '$user' and contraseña = '$password'");
+	if (mysqli_num_rows($result) == 0)
+	{
+		header("Location:login.php?incorrecto=si");
+	}else{
+		session_start();
+		$_SESSION['erablitzailea_a_g'] = $user;
+		header("Location:index.php");
+	}
+?>
