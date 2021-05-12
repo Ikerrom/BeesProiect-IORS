45021a764116ed48b5fde3e52e0213d9931a4eee
Raul617 <75114165+Raul617@users.noreply.github.com>
2021-05-12 09:09:11 +0200

    changes

--- web/account.php
+++ web/account.php
@@ -1,14 +1,20 @@
 <!DOCTYPE html>
 <html>
-	  <head>
-	  	<link rel="stylesheet" href="css.css">
-	  	<link rel="preconnect" href="https://fonts.gstatic.com">
-		<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
-	  	<title>Account</title>
-		  	<style>
-		  			
-		  	</style>
-	  </head>
+	  <head>  
+				<meta charset="utf-8">
+ 				<meta name="viewport" content="width=device-width, initial-scale=1">
+  				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+				<link rel="stylesheet" href="css.css">
+				<title>ERLETE</title>
+				<link rel="preconnect" href="https://fonts.gstatic.com">
+				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500display=swap" rel="stylesheet">
+
+
+
+			</head>
 	  <body>
 
 	  	 <?php
