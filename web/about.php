d090592d2600a321c024d649e912c400819fec17
Raul617 <75114165+Raul617@users.noreply.github.com>
2021-05-12 11:55:01 +0200

    Add files via upload

--- web/about.php
+++ web/about.php
@@ -5,11 +5,10 @@
   				<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   				<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
   				<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
 				<link rel="stylesheet" href="css.css">
 				<title>ERLETE</title>
 				<link rel="preconnect" href="https://fonts.gstatic.com">
-				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500display=swap" rel="stylesheet">
+				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
 
 
 
@@ -78,13 +77,59 @@
 				</div>
 
 
-				<div class="textstyle3">
+			<div class="texttotal">
 
-						EFAAG AFAFS
+				<div class="textstyle">
+					<p><h2>Why did we start working in beekeeping? Where do we sell honey?</h2></p>
+							<br>
+							<br>
+							<p><i><u>Aitor: </u></i></p> My father was already home and I started taking care of the bees. I've been on my way since I was a kid. I share honey among friends and acquaintances.
+							<br><br>
+							<p><i><u>Urdaspal: </u></i></p> Since I had hives about five years ago. The little honey extracted has been for home or for some detail.
+							<br><br>
+							<p><i><u>Ignacio: </u></i></p> Roberto and I have been friends since childhood, and about 35 years ago we started. We sell in the area of Durango, in the square there, and among friends.
+							<br><br>
+							<p><i><u>Hegoi: </u></i></p> This is my third year and, for now, I'm like more honey than I get.
+							<br><br>
+							<p><i><u>Iñigo: </u></i></p> I've been here six to seven years. At work [he's a firefighter] we picked up a lot of bees and since people didn't know what to do, I started taking them home. The honey I take out is for home or for friends. I also take propolis, because it's very good for your health.
+							<br><br>
+							<p><i><u>Felix: </u></i></p> A beekeeper left several hives in Elorrio. I called and started working with him in 2014. Last year I started selling honey at the local market and I've done well. I also sell honey, propolis, pollen and young. It's a nice experience and I'm comfortable.
+							<br><br>
+
 				</div>
 
-			</body>
-		</script>
-	</script>
-</head>
+				<div class="textstyle">
+
+						<br>
+
+
+
+
+						<br><br>
+						<p>						
+						We are currently 14 members of the association, and our sole purpose is to help each other to extract honey, improve the environment for bees, and help them not become extinct.
+						<br><br>	
+						Don't hesitate to contact us in case of any questions.
+						</p>
+
+						<br><br>
+
+						<img src="resources/Images/asociacion.jpg" width="500" height="300">
+						
+					
+				</div>
+			</div>
+			<br><br><br>
+			<div class="texttotal">
+						<img src="resources/Images/telefono.png" style=" width:9%; height:1%;"> <p><font color ="white">Call us at <br>  94 751 34 12</font></p>
+						<br>
+						<br>
+						<img src="resources/Images/correo.png" style="width:9%; height:1%;"> <p><font color ="white">Contact with us at <br>   ErleteAxpe@gmail.com </font></p>
+						<br>
+						<br>
+						<img src="resources/Images/ubicacion.png" style="width:9%; height:1%;"><p><font color ="white"> We are in axpe <br>  San Juan Plaza, 1, 48291 Axpe, Bizkaia </font> </p>
+			</div>
+
+	</body>
+
 </html>
