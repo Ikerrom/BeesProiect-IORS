f1802f03e125497effb5ce2417f7026a82f6752c
Zevallos Márquez <zevallos.julio@uni.lan>
2021-05-19 10:39:38 +0200

    cannot book earlier than today

--- web/book/book.php
+++ web/book/book.php
@@ -22,43 +22,49 @@ if (!isset($_GET['x'])) {
     $arr = json_decode($_GET['x']);
 }
 if (isset($arr->date) && property_exists($arr, 'lataid')) {
-    $dt = (new DateTime())->format('Y-m-d H:i:s');
-    try {
-        if ($arr->lataid === null) {
-            $stmt = $conn->prepare("INSERT INTO Reservas (dni, dia_reservado, lata_id,"
-                    . " dia_dereserva) VALUES (?, ?, NULL, ?)");
-            $stmt->bind_param("sss", $dni, $arr->date, $dt);
-        } else {
-            $stmt = $conn->prepare("INSERT INTO Reservas (dni, dia_reservado, lata_id,"
-                    . " dia_dereserva) VALUES (?, ?, ?, ?)");
-            $stmt->bind_param("ssis", $dni, $arr->date, $arr->lataid, $dt);
-        }
-
-        $stmt->execute();
-        $stmt->close();
-        if ($arr->lataid === null) {
-            $ret->eginda = true;
-        } else {
-            $stmt2 = $conn->prepare("SELECT dni, dia_dereserva FROM Reservas "
-                    . "WHERE lata_id = ? AND dia_reservado BETWEEN ? AND ? "
-                    . "ORDER BY dia_dereserva ASC, dni");
-            $booking = get_booked($arr);
-            $stmt2->bind_param('iss', $arr->lataid, $booking['from'], $booking['until']);
-            $stmt2->execute();
-            $stmt2->bind_result($firstdni, $firsttime);
-            if ($stmt2->fetch() && $firstdni === $dni && $firsttime === $dt) {
-                $ret->eginda = true;
-                $stmt2->close();
+    $cdt = new DateTime();
+    $rdt = DateTime::createFromFormat('Y-m-d', $arr->date);
+    if ($rdt > $cdt) {
+        $sdt = ($cdt)->format('Y-m-d H:i:s');
+        try {
+            if ($arr->lataid === null) {
+                $stmt = $conn->prepare("INSERT INTO Reservas (dni, dia_reservado, lata_id,"
+                        . " dia_dereserva) VALUES (?, ?, NULL, ?)");
+                $stmt->bind_param("sss", $dni, $arr->date, $sdt);
             } else {
-                $stmt2->close();
-                $conn->rollback();
-                $ret->ezlata = true;
+                $stmt = $conn->prepare("INSERT INTO Reservas (dni, dia_reservado, lata_id,"
+                        . " dia_dereserva) VALUES (?, ?, ?, ?)");
+                $stmt->bind_param("ssis", $dni, $arr->date, $arr->lataid, $sdt);
             }
+
+            $stmt->execute();
+            $stmt->close();
+            if ($arr->lataid === null) {
+                $ret->eginda = true;
+            } else {
+                $stmt2 = $conn->prepare("SELECT dni, dia_dereserva FROM Reservas "
+                        . "WHERE lata_id = ? AND dia_reservado BETWEEN ? AND ? "
+                        . "ORDER BY dia_dereserva ASC, dni");
+                $booking = get_booked($arr);
+                $stmt2->bind_param('iss', $arr->lataid, $booking['from'], $booking['until']);
+                $stmt2->execute();
+                $stmt2->bind_result($firstdni, $firsttime);
+                if ($stmt2->fetch() && $firstdni === $dni && $firsttime === $sdt) {
+                    $ret->eginda = true;
+                    $stmt2->close();
+                } else {
+                    $stmt2->close();
+                    $conn->rollback();
+                    $ret->ezlata = true;
+                }
+            }
+        } catch (mysqli_sql_exception $e) {
+            $stmt->close();
+            $conn->rollback();
+            $ret->ezinda = true;
         }
-    } catch (mysqli_sql_exception $e) {
-        $stmt->close();
-        $conn->rollback();
-        $ret->ezinda = true;
+    } else {
+        $ret->berandu = true;
     }
 }
 
