69dc4fe051860fbe79094b6f41c8551b0bb9c80d
Raul617 <75114165+Raul617@users.noreply.github.com>
2021-05-19 12:44:08 +0200

    changes

--- web/upload.php
+++ web/upload.php
@@ -1,5 +1,7 @@
 <?php
-
+		/* Para que al darle al submit en your account
+		 la imagen se guarde y se cambie, guarda
+		 la direccion de la imagen en la base de datos */
 	$dir = "resources/images/perfiles";
 	$imagen =$_FILES['imagen']['name'];
 	$archivo = $_FILES['imagen']['tmp_name'];
@@ -7,11 +9,12 @@
 	move_uploaded_file($archivo, $dir);
 
 	session_start();
-	if (isset($_SESSION['erablitzailea_a_g'])) {
+	if (isset($_SESSION['erablitzailea_a_g'])) { 
 		include("test_connect_db.php");
 		$dni = $_SESSION['erablitzailea_a_g'];
 		$link =  ConnectDataBase();
-		$result=mysqli_query($link, "UPDATE Personas SET Foto = '$dir' WHERE dni = '$dni'"); 
+		$result=mysqli_query($link, "UPDATE Personas SET Foto = '$dir' WHERE dni = '$dni'");   /* Update para guardar en la base de datos 
+																								  la nueva imagen de perfil*/
 		header("Location:account.php");
 		}
 ?>
